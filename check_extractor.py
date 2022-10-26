bf05cc1d694135b2cb217a8fe473066729d68ac3
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2022-10-26 12:42:34 +0300

    check extractor works

---
+++ check_extractor.py
@@ -0,0 +1,33 @@
+import project
+import numpy as np
+import torch
+from FrameGenrator import FrameGenerator
+from transforms import GroupNormalize, GroupScale, GroupCenterCrop
+from torchvision import transforms
+import torchvision
+
+device = torch.device('cuda')
+video_path = "/data/shared-data/scalpel/APAS-Activities/data/APAS/frames/P016_balloon1_side"
+
+
+batch_input = np.load(
+    "/data/shared-data/scalpel/APAS-Activities/data/APAS/features/fold 2/P016_balloon1.npy")
+batch_input = torch.tensor(batch_input).to(device)
+batch_input = batch_input.reshape(1, *batch_input.shape)
+print(batch_input.shape)
+
+_, extractor = project.prepare_models(use_accelerator=False)
+fg = FrameGenerator(video_path)
+
+val_augmentation = transforms.Compose([GroupScale(int(256)),
+                                       GroupCenterCrop(224)])
+normalize = GroupNormalize(extractor.input_mean, extractor.input_std)
+img = fg.next()
+
+frame_tensor = val_augmentation([img])
+frame_tensor = transforms.ToTensor()(frame_tensor[0])
+frame_tensor = frame_tensor.to(device)
+frame_tensor = normalize(frame_tensor)
+frame_tensor = frame_tensor.view(1, *frame_tensor.size())
+features = extractor(frame_tensor)[1]
+print(torch.norm(features - batch_input[:, :, 0]))
--- try1.py
+++
@@ -1,9 +0,0 @@
-from tqdm import tqdm as t
-import time
-import math
-
-pbar = t(total=math.inf, unit=' frames',
-         bar_format='Calculated {n} frames. current rate: {rate_fmt}')
-for _ in range(1000):
-    time.sleep(0.023)
-    pbar.update(1)
